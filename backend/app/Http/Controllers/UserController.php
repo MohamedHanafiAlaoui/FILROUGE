3af8d4a2946c7e32d12c45d4340aef0cd5742060
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-03-16 08:47:00 +0000

    ajouter function authentifier

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|max:255|unique:users,email',
+            'password'=>'required|string|min:8|confirmed',
+        ]);
+
+       $user= User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+       ]);
+       return response()->json(['message'=>'user registered Succeccfully','user'=>$user],201);
+
+    }
+
+    public function login(request $request)
+    {
+        $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|string',
+        ]);
+        if (!Auth::attempt($request->only('email','password'))) 
+        return response()->json(['message' => 'invalid email or password'], 401);
+
+       $user= User::where('email',$request->email)->firstOrFail();
+       $token=$user->createToken('auth_Token')->plainTextToken;
+       return response()->json(['message'=>'user login Succeccfully','user'=>$user,'token'=>$token],201);
+
+    }
+
+    public function logout(request $request)
+    {
+
+        $request->user()->currentAccessToken()->delete;
+       return response()->json(['message'=>'user Logout Succeccfully']);
+
+        
+    }
+
+}
