f972ed37df0381544a166ce8a27a54102c6080a8
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-04-11 16:58:13 +0100

    ajouter api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CalendarEntryController;
+use App\Http\Controllers\CalendrierController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,3 +17,12 @@ Route::get('/user', function (Request $request) {
 Route ::post('login',[UserController::class, 'login']);
 Route ::post('register',[UserController::class, 'register']);
 Route ::post('logout',[UserController::class, 'logout'])->middleware('auth:sanctum');
+
+
+Route ::post('Calendrier',[CalendrierController::class, 'store']);
+Route ::get('Calendrier/',[CalendrierController::class, 'index']);
+
+
+
+Route ::get('Calendrier/{id}',[CalendrierController::class, 'show']);
+Route ::get('CalendarEntry/{id}',[CalendarEntryController::class, 'index']);
