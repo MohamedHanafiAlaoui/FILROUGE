96a45b2afdce5f87fa75535b5d9fb36da76e9961
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-04-22 20:52:19 +0100

    user-is-online

--- FilROUGE2/app/Models/User.php
+++ FilROUGE2/app/Models/User.php
@@ -64,4 +64,10 @@ class User extends Authenticatable
         return $this->hasMany(SolutionsAdaptees::class, 'id_agricole');
     }
 
+
+    public function isOnline()
+    {
+        return cache()->has('user-is-online')
+    }
+
 }
