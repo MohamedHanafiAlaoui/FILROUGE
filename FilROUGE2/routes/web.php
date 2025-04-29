6fb7eea5e3ce48951aaf6ee9094bc0e34dc5cd95
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-04-29 11:22:38 +0100

    ajout des routes pour afficher, ajouter et modifier les étapes du calendrier

--- FilROUGE2/routes/web.php
+++ FilROUGE2/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AgricoleSolutionsAdapteesController;
 use App\Http\Controllers\CalendrierController;
+use App\Http\Controllers\SignalerController;
 use App\Http\Controllers\SolutionsAdapteesController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -11,10 +12,7 @@ use App\Http\Controllers\CalendarEntryController;
 
 Route::get('/inscription', [UserController::class, 'showRegisterForm'])->name('inscription');
 Route::post('/inscription', [UserController::class, 'register'])->name('inscription');
-
 Route::get('/connexion', [UserController::class, 'showLoginForm'])->name('login');
-
-
 Route::post('/connexion', [UserController::class, 'login'])->name('login.submit');
 
 
@@ -23,10 +21,28 @@ Route::get('/', function () {
 });
 
 
-Route::get('/agriculteur/Calendrier', [CalendrierController::class, 'index'])->name('agriculteur.Calendrier');
 
-// Route::get('/agriculteur/etapes/{id}', [CalendarEntryController::class, 'index'])->name('agriculteur.etapes');
-Route::get('/agriculteur/etapes/{id}', [CalendarEntryController::class, 'index'])->name('agriculteur.etapes');
+
+Route::middleware(['auth'])->group(function () {
+    // Calendar routes
+    Route::get('agriculteur/Calendrier', [CalendrierController::class, 'index'])->name('agriculteur.Calendrier');
+    Route::post('agriculteur/Calendrier', [CalendrierController::class, 'store'])->name('calendrier.store');
+    Route::get('agriculteur/Calendrier/{id}', [CalendrierController::class, 'show'])->name('calendrier.show');
+    Route::put('agriculteur/Calendrier/update/{id}', [CalendrierController::class, 'update'])->name('calendrier.update');
+    Route::delete('agriculteur/calendrier/delete/{id}', [CalendrierController::class, 'destroy'])->name('calendrier.destroy');
+
+
+    Route::get('/agriculteur/entries/{id}', [CalendarEntryController::class, 'index'])->name('calendar.entries');
+    Route::post('/agriculteur/entries', [CalendarEntryController::class, 'store'])->name('calendar.entries.store');
+    Route::put('/agriculteur/entries/update/{id}', [CalendarEntryController::class, 'update'])->name('calendar.entries.update');
+
+
+
+});
+
+
+
+
 
 
 Route::middleware('auth')->group(function()
@@ -53,3 +69,7 @@ Route::middleware('auth')->group(function()
     Route::post('/agricole/offline', [AgricoleSolutionsAdapteesController::class,'setOffline']);
 
 });
+
+
+
+Route::resource('signalers', SignalerController::class);
