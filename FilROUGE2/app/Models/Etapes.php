a887f1d742899a17181d70a38b5c7e2bc3cfb40d
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-05-07 21:24:18 +0100

    Ajout de la relation hasMany avec  dans le modèle Etapes

--- FilROUGE2/app/Models/Etapes.php
+++ FilROUGE2/app/Models/Etapes.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Etapes extends Model
 {
-    //
+    public function calendarEntries()
+    {
+        return $this->hasMany(CalendarEntry::class, 'id_etapes');
+    }
 }
