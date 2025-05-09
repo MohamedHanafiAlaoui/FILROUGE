c735f85f2eaeb7d5775ebce14e009418d5827204
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-05-09 21:18:04 +0100

    ajoter logout

--- FilROUGE2/app/Http/Controllers/UserController.php
+++ FilROUGE2/app/Http/Controllers/UserController.php
@@ -68,6 +68,8 @@ class UserController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return redirect()->route('login')->with('success', 'Déconnexion réussie.');
     }
 }
