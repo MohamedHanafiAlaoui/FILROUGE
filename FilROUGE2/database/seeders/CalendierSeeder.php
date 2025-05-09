697d713685355634f84205a50db6dc0d7f853256
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-05-09 21:24:03 +0100

    modfer data seeder

--- FilROUGE2/database/seeders/CalendierSeeder.php
+++ FilROUGE2/database/seeders/CalendierSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Calendrier;
+use App\Models\Cultures;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,41 +14,146 @@ class CalendierSeeder extends Seeder
      */
     public function run(): void
     {
+
         $calendriers = [
             [
-                'image' => 'images/calendrier1.jpg',
-                'name' => 'Plantation de blé',
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation de carottes',
                 'id_agriculteur' => 3,
-                'etapes' => '1'
+                'etapes' => 'Semis'
             ],
             [
-                'image' => 'images/calendrier2.jpg',
-                'name' => 'Irrigation du maïs',
-                'id_agriculteur' =>3,
-                'etapes' => '2'
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Irrigation des fraises',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
             ],
             [
-                'image' => 'images/calendrier3.jpg',
-                'name' => 'Récolte des olives',
-                'id_agriculteur' =>3,
-                'etapes' => '3'
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Récolte des pommes',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
             ],
             [
-                'image' => 'images/calendrier4.jpg',
-                'name' => 'Fertilisation des tomates',
-                'id_agriculteur' =>3,
-                'etapes' => '1'
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de maïs',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
             ],
             [
-                'image' => 'images/calendrier5.jpg',
-                'name' => 'Protection contre les parasites',
-                'id_agriculteur' => 6,
-                'etapes' => '2'
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation de salade',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
             ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de haricots',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation de courgettes',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de betteraves',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de tournesols',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation de choux',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de concombres',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation d\'aubergines',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de poivrons',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation de patates douces',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis d\'épinards',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation de brocolis',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de céleri',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation d\'artichauts',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de radis',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation d\'oignons',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Semis de persil',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ],
+            [
+                'image' => 'https://images.unsplash.com/photo-1582284540020-8acbe03f4924',
+                'name' => 'Plantation de thym',
+                'id_agriculteur' => 3,
+                'etapes' => 'Semis'
+            ]
         ];
+
+
     foreach($calendriers as  $calendrier)
     {
-        Calendrier::create($calendrier);
+            Cultures::create($calendrier);
 
     };
 
